76b7c25b177f10c4118c41f37308b55fedd5667e
Rangel <breno.r.ramos1@gmail.com>
2026-05-18 11:45:19 -0300

    feat: implement light mode and color themes for calculator

--- js/calculator.js
+++ js/calculator.js
@@ -1,14 +1,74 @@
-let light_button = document.querySelector('#light-button')
-console.log(light_button)
-let sunny_icon = document.querySelector('#sunny-icon')
+let body = document.body
+
+let theme_button = {
+    light_night: document.querySelector('#light-button'),
+    sunny_icon: document.querySelector('#sunny-icon'),
+    pink: document.querySelector('#pink-theme'),
+    blue: document.querySelector('#blue-theme'),
+    purple: document.querySelector('#purple-theme'),
+    default: document.querySelector('#default-theme')
+}
+
+/*
+function hasAnyClass(element, classes) {
+    return classes.some(function(className) {
+        return element.classList.contains(className)
+    })
+}
+*/
 
 // FUNCTION LIGHT/DARK THEME
-light_button.addEventListener('click', function(){
-    if (document.body.className != 'light'){
-        document.body.classList.add('light')
-        sunny_icon.innerHTML = 'nightlight'
+theme_button.light_night.addEventListener('click', function () {
+    if (!body.classList.contains('light')) {
+        body.classList.add('light')
+        theme_button.sunny_icon.innerHTML = 'nightlight'
     } else {
-        document.body.classList.remove('light')
-        sunny_icon.innerHTML = 'sunny'
+        body.classList.remove('light')
+        theme_button.sunny_icon.innerHTML = 'sunny'
+    }
+})
+
+// FUNTION PINK THEME
+theme_button.pink.addEventListener('click', function () {
+    if (!body.classList.contains('pink')) {
+        body.classList.add('pink')
+        body.classList.remove('blue')
+        body.classList.remove('purple')
+    } else {
+        body.classList.remove('pink')
+    }
+})
+
+// FUNCTION BLUE THEME
+theme_button.blue.addEventListener('click', function () {
+    if (!body.classList.contains('blue')) {
+        body.classList.add('blue')
+        body.classList.remove('pink')
+        body.classList.remove('purple')
+    } else {
+        body.classList.remove('blue')
+    }
+})
+
+// FUNCTION PURPLE THEME
+theme_button.purple.addEventListener('click', function () {
+    if (!body.classList.contains('purple')) {
+        body.classList.add('purple')
+        body.classList.remove('pink')
+        body.classList.remove('blue')
+    } else {
+        body.classList.remove('purple')
+    }
+})
+
+// FUNCTION RESETE TO DEFAULT THEME
+theme_button.default.addEventListener('click', function () {
+    if (body.classList.contains('pink') ||
+        body.classList.contains('blue') ||
+        body.classList.contains('purple')) {
+
+        body.classList.remove('pink')
+        body.classList.remove('blue')
+        body.classList.remove('purple')
     }
 })
