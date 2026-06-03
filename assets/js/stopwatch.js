c7c8466e482cf2521184fafce644f94db3139927
Rangel <breno.r.ramos1@gmail.com>
2026-06-03 19:43:54 -0300

    feat: add reset stopwatch function

--- assets/js/stopwatch.js
+++ assets/js/stopwatch.js
@@ -34,14 +34,16 @@
     let currentStopWatch = null
     const stopwatchSection = document.querySelector('.stopwatch')
 
+    const button_LepReset = document.querySelector('.lap-reset-button')
     // FUNCTION STOPWATCH
-    const start_Button = document.querySelector('.start-button')
-    start_Button.addEventListener('click', function () {
+    const button_Start = document.querySelector('.start-button')
+    button_Start.addEventListener('click', function () {
         this.classList.toggle('stop-button')
 
         if (this.classList.contains('stop-button')) {
             this.innerText = 'stop'
-            
+            button_LepReset.classList.add('active')
+            button_LepReset.innerText = 'Lap'
 
             currentStopWatch = setInterval(function () {
                 miliseconds += 10
@@ -50,7 +52,17 @@
             }, 10)
         } else {
             this.innerText = 'start'
+            button_LepReset.innerText = 'Reset'
             clearInterval(currentStopWatch)
+        }
+    })
+
+    // FUNCTION RESET
+    button_LepReset.addEventListener('click', function () {
+        if (this.innerText === 'Reset') {
+            clearInterval(currentStopWatch)
+            miliseconds = 0
+            stopwatchSection.innerText = '00:00,00'
         }
     })
 })()
