7db92b130fa09c833cb8f4cb9ce998cd78899515
Rangel <breno.r.ramos1@gmail.com>
2026-06-03 15:45:31 -0300

    feat: add getTime & setTime on clock

--- assets/js/stopwatch.js
+++ assets/js/stopwatch.js
@@ -1,4 +1,17 @@
 (function myScope() {
 
-    
-})
+    function getTime(clock) {
+        const today = new Date()
+        const time = today.toLocaleTimeString('pt-BR', {
+            timeStyle: 'short'
+        }) 
+        return clock.innerText = time
+    }
+
+    function setTime() {
+        const clock = document.querySelector('.clock')
+        getTime(clock)
+    }
+    setTime()
+    setInterval(setTime, 1000)
+})()
